434e0cad39c034b1c6b11cecaacbcfb03505e170
zhocean <youshanghaiyang@gmail.com>
2021-02-17 20:04:37 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { App, Plugin } from 'vue';
 import getIcon from './getIcon';
 
 const IconPlugin: Plugin = {
-  install(app: App, options: any = {}, ...rest: any[]) {
+  install(app: App, options: any = {}) {
     const tagName = options.tag || 'icon'
     if (!app.component(tagName)) {
       app.component(tagName, getIcon(options))
